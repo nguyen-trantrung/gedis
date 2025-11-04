72a90d73942d0a2842ba8b7871203450676af340
Nguyen Tran (Mark) <nguyen.trantrung@navercorp.com>
2025-11-04 16:45:41 +0700

    allow specifying host and port

--- app/main.go
+++ app/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"context"
+	"flag"
 
 	"github.com/ttn-nguyen42/gedis/server"
 )
 
 func main() {
 	ctx := server.CancelOnSignal(context.Background())
-	s := server.NewServer("localhost", 6379)
+	s := getServer()
 	if err := s.Run(ctx); err != nil {
 		panic(err)
 	}
+}
+
+func getServer() *server.Server {
+	var port int
+	flag.IntVar(&port, "port", 6379, "Specify port to use")
+
+	var host string
+	flag.StringVar(&host, "host", "0.0.0.0", "Specify hostname to use")
+
+	flag.Parse()
+
+	return server.NewServer(host, port)
 }
